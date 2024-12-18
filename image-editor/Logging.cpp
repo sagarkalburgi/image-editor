2e4638170338cbd8ea094f526b2f45f49c3c2ee2
Sagar Kalburgi <kalburgi@clkgmbh.de>
2024-12-18 15:37:39 +0100

    logging files added

---
+++ image-editor/Logging.cpp
@@ -0,0 +1,5 @@
+#include "logging.h"
+
+// Define logging categories
+Q_LOGGING_CATEGORY(imageEditorLog, "imageeditor")
+Q_LOGGING_CATEGORY(dialogLog, "dialog")
---
+++ image-editor/logging.h
@@ -0,0 +1,10 @@
+#ifndef LOGGING_H
+#define LOGGING_H
+
+#include <QLoggingCategory>
+
+// Define logging categories
+Q_DECLARE_LOGGING_CATEGORY(imageEditorLog)
+Q_DECLARE_LOGGING_CATEGORY(dialogLog)
+
+#endif // LOGGING_H
